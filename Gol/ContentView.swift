b7af03cc608bcb96cf04f5e40393dc0ac35a9001
Raphael Cerqueira <raphaelfc@gmail.com>
2021-04-28 20:20:30 -0300

    Primeiro commit

--- Gol/ContentView.swift
+++ Gol/ContentView.swift
@@ -8,9 +8,184 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showDetailsView: Bool = false
+    @Namespace private var animation
+    @State var detailsTitle: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            if !showDetailsView {
+                HomeView(showDetailsView: $showDetailsView, animation: animation, detailsTitle: $detailsTitle)
+            }
+            
+            if showDetailsView {
+                DetailsView(showDetailsView: $showDetailsView, animation: animation, detailsTitle: $detailsTitle)
+            }
+        }
+    }
+}
+
+struct HomeView: View {
+    @Binding var showDetailsView: Bool
+    var animation: Namespace.ID
+    
+    let screen = UIScreen.main.bounds
+    
+    let options = ["Minhas viagens", "Check-in", "Comprar viagem", "Minha conta", "Status de voo"]
+    
+    let moreOptions = ["Voe Junto", "Ajuda", "Sair"]
+    
+    @Binding var detailsTitle: String
+    
+    @State var moreOptionsVisible: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9924781919, green: 0.3595064878, blue: 0.00949444063, alpha: 1)), Color(#colorLiteral(red: 0.9723210931, green: 0.5567080975, blue: 0.06823665649, alpha: 1))]), startPoint: .bottom, endPoint: .top))
+                .matchedGeometryEffect(id: "background", in: animation)
+                .frame(width: calculateRadius(), height: calculateRadius())
+            
+            VStack(alignment: .leading) {
+                Text("Olá, Raphael")
+                    .padding(20)
+                    .padding(.top, 20)
+                
+                Spacer()
+                
+                Text("Você ainda não tem nenhuma viagem marcada")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 20)
+                
+                HStack {
+                    Button(action: {
+                        showView("Comprar viagem")
+                    }, label: {
+                        Text("Comprar")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color(#colorLiteral(red: 0.9924781919, green: 0.3595064878, blue: 0.00949444063, alpha: 1)))
+                            .padding(.vertical, 12)
+                            .padding(.horizontal, 30)
+                            .background(Capsule().fill(Color.white))
+                    })
+                    
+                    Button(action: {
+                        showView("Localizar viagem")
+                    }, label: {
+                        Text("Adicionar viagem")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal, 30)
+                            .background(Capsule().strokeBorder(Color.white, lineWidth: 3))
+                    })
+                }
+                .padding(20)
+                
+                Spacer()
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        ForEach(options, id: \.self) { option in
+                            Button(action: {
+                                showView(option)
+                            }, label: {
+                                Text(option)
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                    .padding(.bottom)
+                            })
+                        }
+                        
+                        if moreOptionsVisible {
+                            VStack(alignment: .leading) {
+                                ForEach(moreOptions, id: \.self) { option in
+                                    Button(action: {
+                                        showView(option)
+                                    }, label: {
+                                        Text(option)
+                                            .font(.title)
+                                            .foregroundColor(Color.white.opacity(0.7))
+                                            .padding(.bottom)
+                                    })
+                                }
+                            }
+                        }
+                        
+                        Button(action: {
+                            withAnimation {
+                                moreOptionsVisible.toggle()
+                            }
+                        }, label: {
+                            HStack {
+                                Text(moreOptionsVisible ? "Ocultar" : "Mais opções")
+                                    .font(.title)
+                                    .foregroundColor(Color.white.opacity(0.7))
+                                
+                                Image(systemName: moreOptionsVisible ? "chevron.up" : "chevron.down")
+                            }
+                        })
+                    }
+                    
+                    Spacer()
+                }
+                .padding(20)
+                .padding(.bottom, 40)
+            }
+            .foregroundColor(.white)
+            .frame(width: screen.width, height: screen.height)
+        }
+    }
+    
+    func showView(_ title: String) {
+        detailsTitle = title
+        withAnimation {
+            showDetailsView.toggle()
+        }
+    }
+    
+    func calculateRadius() -> CGFloat {
+        let halfWidth = screen.width / 2
+        let halfHeight = screen.height / 2
+        let radius = (halfWidth * halfWidth) + (halfHeight * halfHeight)
+        return sqrt(radius) * 2
+    }
+}
+
+struct DetailsView: View {
+    @Binding var showDetailsView: Bool
+    var animation: Namespace.ID
+    @Binding var detailsTitle: String
+    
+    var body: some View {
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Text(detailsTitle)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+        
+                    Button(action: {
+                        withAnimation {
+                            showDetailsView.toggle()
+                        }
+                    }, label: {
+                        Circle()
+                            .matchedGeometryEffect(id: "background", in: animation)
+                            .foregroundColor(Color(#colorLiteral(red: 0.9924781919, green: 0.3595064878, blue: 0.00949444063, alpha: 1)))
+                            .frame(width: 60, height: 60)
+                    })
+                }
+                .padding()
+                
+                Spacer()
+            }
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,23 @@
+# Aplicativo da Gol usando SwiftUI
+Tela principal do aplicativo da Gol (https://voegol.com.br) usando SwiftUI no iOS14.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/116484027-a2674080-a85e-11eb-8ca7-5ff77670d7f3.png" width="400"> &nbsp;
+</p>
+
+## Vídeos
+- [Vídeo no YouTube](https://youtu.be/cORfjcOax9U)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)
